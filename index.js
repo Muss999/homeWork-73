bd4e75cd440c439281904c0fb3af3200e9240bdd
Musulmankul <mussangle1@gmail.com>
2025-08-07 13:32:40 +0600

    finished second task

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const Vigenere = require("caesar-salad").Vigenere;
+
+const PASSWORD = "password";
 
 const app = express();
 const port = 8000;
 
 app.get("/:text", (req, res) => {
-    return res.send(`${req.params.text}`);
+    return res.send(req.params.text);
+});
+
+app.get("/encode/:text", (req, res) => {
+    return res.send(Vigenere.Cipher(PASSWORD).crypt(req.params.text));
+});
+
+app.get("/decode/:text", (req, res) => {
+    return res.send(Vigenere.Decipher(PASSWORD).crypt(req.params.text));
 });
 
 app.listen(port, () => {
